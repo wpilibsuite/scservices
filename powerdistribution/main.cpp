dc2a5341be599a590af903c31ae00852780356f4
Thad House <thadhouse1@gmail.com>
2025-01-27 20:17:34 -0800

    Revert kitcan back to just power distribution
    The vendor plugins should be handling version checking. Required for CTRE, so might as well be for Rev too

--- powerdistribution/main.cpp
+++ powerdistribution/main.cpp
@@ -16,29 +16,16 @@
 #include "networktables/NetworkTableInstance.h"
 #include "networktables/RawTopic.h"
 #include "networktables/IntegerTopic.h"
-#include "networktables/StringArrayTopic.h"
-#include "networktables/BooleanTopic.h"
 
-#define NUM_CAN_BUSES 5
+#define NUM_CAN_BUSES 2
 
 static constexpr uint32_t deviceTypeMask = 0x3F000000;
 static constexpr uint32_t powerDistributionFilter = 0x08000000;
-static constexpr uint32_t pneumaticsFilter = 0x09000000;
-static constexpr uint32_t manufacturerMask = 0x00FF0000;
-static constexpr uint32_t ctreFilter = 0x00040000;
-static constexpr uint32_t revFilter = 0x00050000;
-
-constexpr uint32_t revPhVersionPacketMask = 0x09052600;
-constexpr uint32_t revPhAnyPacketMask = 0x09050000;
-constexpr uint32_t revPdhVersionPacketMask = 0x08052600;
 
 struct CanState {
-    wpi::mutex sendVersionsMutex;
     int socketHandle{-1};
     nt::IntegerPublisher deviceIdPublisher;
     std::array<nt::RawPublisher, 4> framePublishers;
-    nt::StringArrayPublisher versionPublisher;
-    std::unordered_map<uint32_t, std::string> sentVersions;
     unsigned busId{0};
 
     ~CanState() {
@@ -47,84 +34,11 @@ struct CanState {
         }
     }
 
-    void maybeSendVersionRequest(uint32_t canId);
-
-    void handleRevVersionFrame(const canfd_frame& frame);
-    void sendVersions();
-
     void handleCanFrame(const canfd_frame& frame);
     void handlePowerFrame(const canfd_frame& frame);
-    void handlePneumaticsFrame(const canfd_frame& frame);
     bool startUvLoop(unsigned bus, const nt::NetworkTableInstance& ntInst,
                      wpi::uv::Loop& loop);
 };
-
-void CanState::sendVersions() {
-    std::scoped_lock lock{sendVersionsMutex};
-
-    for (auto&& versions : sentVersions) {
-        if (versions.first == 0 || versions.second.empty()) {
-            continue;
-        }
-
-        uint32_t frameId = versions.first;
-
-        std::array<std::string, 3> sendData;
-        sendData[0] = std::to_string(frameId);
-        sendData[1] = ((frameId & deviceTypeMask) == pneumaticsFilter)
-                          ? "Rev PH"
-                          : "Rev PDH";
-        sendData[2] = versions.second;
-
-        fmt::print("Setting {:x} {} {}\n", frameId, sendData[1], sendData[2]);
-
-        versionPublisher.Set(sendData);
-    }
-}
-
-void CanState::maybeSendVersionRequest(uint32_t canId) {
-    auto& sent = sentVersions[canId];
-    if (!sent.empty()) {
-        return;
-    }
-
-    canfd_frame frame;
-    memset(&frame, 0, sizeof(frame));
-    frame.can_id = canId | CAN_EFF_FLAG | CAN_RTR_FLAG;
-    frame.len = 8;
-
-    printf("Requesting %x version frame\n", canId);
-
-    send(socketHandle, &frame, CAN_MTU, 0);
-}
-
-void CanState::handleRevVersionFrame(const canfd_frame& frame) {
-    if (frame.len < 8) {
-        return;
-    }
-
-    uint8_t year = frame.data[2];
-    uint8_t minor = frame.data[1];
-    uint8_t fix = frame.data[0];
-
-    uint32_t frameId = frame.can_id & CAN_EFF_MASK;
-
-    char buf[32];
-    snprintf(buf, sizeof(buf), "%u.%u.%u", year, minor, fix);
-
-    std::scoped_lock lock{sendVersionsMutex};
-    sentVersions[frameId] = buf;
-}
-
-void CanState::handlePneumaticsFrame(const canfd_frame& frame) {
-    // The only thing we're doing with pneumatics is version receiving.
-
-    if ((frame.can_id & revPhVersionPacketMask) == revPhVersionPacketMask) {
-        handleRevVersionFrame(frame);
-    } else if ((frame.can_id & revPhAnyPacketMask) == revPhAnyPacketMask) {
-        maybeSendVersionRequest(revPhVersionPacketMask | (frame.can_id & 0x3F));
-    }
-}
 
 void CanState::handleCanFrame(const canfd_frame& frame) {
     // Can't support FD frames
@@ -138,8 +52,6 @@ void CanState::handleCanFrame(const canfd_frame& frame) {
 
     if (maskedDeviceType == powerDistributionFilter) {
         handlePowerFrame(frame);
-    } else if (maskedDeviceType == pneumaticsFilter) {
-        handlePneumaticsFrame(frame);
     }
 }
 
@@ -151,30 +63,21 @@ void CanState::handlePowerFrame(const canfd_frame& frame) {
 
     if (frame.can_id & 0x10000) {
         // Rev Frame
-        if (apiId == 0x98) {
-            // Version frame
-            handleRevVersionFrame(frame);
-            return;
-        } else if (apiId < 0x60 || apiId > 0x63) {
+        if (apiId < 0x60 || apiId > 0x63) {
             // Not valid
             return;
         }
 
-        maybeSendVersionRequest(revPdhVersionPacketMask | (deviceId & 0x3F));
-
         frameNum = apiId - 0x60;
     } else {
         // CTRE frame
-
         if (apiId == 0x5D) {
             // Special case
             frameNum = 3;
+        } else if (apiId < 0x50 || apiId > 0x52) {
+            // Not valid
+            return;
         } else {
-            if (apiId < 0x50 || apiId > 0x52) {
-                // Not valid
-                return;
-            }
-
             frameNum = apiId - 0x50;
         }
     }
@@ -194,7 +97,7 @@ void CanState::handlePowerFrame(const canfd_frame& frame) {
 
 bool CanState::startUvLoop(unsigned bus, const nt::NetworkTableInstance& ntInst,
                            wpi::uv::Loop& loop) {
-    if (busId >= NUM_CAN_BUSES) {
+    if (bus >= NUM_CAN_BUSES) {
         return false;
     }
 
@@ -217,10 +120,6 @@ bool CanState::startUvLoop(unsigned bus, const nt::NetworkTableInstance& ntInst,
     deviceIdPublisher =
         ntInst.GetIntegerTopic("/pd/" + busIdStr + "/deviceid").Publish();
 
-    versionPublisher =
-        ntInst.GetStringArrayTopic("/Netcomm/Reporting/UserVersionStr")
-            .Publish(options);
-
     socketHandle =
         socket(PF_CAN, SOCK_RAW | SOCK_NONBLOCK | SOCK_CLOEXEC, CAN_RAW);
 
@@ -228,14 +127,13 @@ bool CanState::startUvLoop(unsigned bus, const nt::NetworkTableInstance& ntInst,
         return false;
     }
 
-    // Filter to PD or pneumatics device type.
+    // Filter to PD device type.
     // Both mfg types have the "4" bit set. They just
     // differ on the 1 bit. So a single filter can be used,
     // ignoring that bit.
-    // Same thing for 8 vs 9 for the device type
     struct can_filter filter {
         .can_id = 0x08040000 | CAN_EFF_FLAG,
-        .can_mask = 0x1EFE0000 | CAN_EFF_FLAG,
+        .can_mask = 0x1FFE0000 | CAN_EFF_FLAG,
     };
 
     if (setsockopt(socketHandle, SOL_CAN_RAW, CAN_RAW_FILTER, &filter,
@@ -294,7 +192,7 @@ bool CanState::startUvLoop(unsigned bus, const nt::NetworkTableInstance& ntInst,
 }
 
 int main() {
-    printf("Starting KitCanDaemon\n");
+    printf("Starting PowerDistributionDaemon\n");
     printf("\tBuild Hash: %s\n", MRC_GetGitHash());
     printf("\tBuild Timestamp: %s\n", MRC_GetBuildTimestamp());
 
@@ -310,11 +208,7 @@ int main() {
 
     auto ntInst = nt::NetworkTableInstance::Create();
     ntInst.SetServer({"localhost"}, 6810);
-    ntInst.StartClient("KitCanDaemon");
-
-    nt::IntegerSubscriber requestSubscriber =
-        ntInst.GetIntegerTopic("/Netcomm/Reporting/RequestVersions")
-            .Subscribe(0);
+    ntInst.StartClient("PowerDistributionDaemon");
 
     wpi::EventLoopRunner loopRunner;
 
@@ -327,14 +221,6 @@ int main() {
             }
         }
     });
-
-    NT_Listener requestListener =
-        ntInst.AddListener(requestSubscriber, NT_EVENT_VALUE_REMOTE,
-                           [&states, &loopRunner](const nt::Event& event) {
-                               for (size_t i = 0; i < states.size(); i++) {
-                                   states[i].sendVersions();
-                               }
-                           });
 
     if (!success) {
         loopRunner.Stop();
@@ -349,7 +235,6 @@ int main() {
         (void)getchar();
 #endif
     }
-    ntInst.RemoveListener(requestListener);
     ntInst.StopClient();
     nt::NetworkTableInstance::Destroy(ntInst);
 
